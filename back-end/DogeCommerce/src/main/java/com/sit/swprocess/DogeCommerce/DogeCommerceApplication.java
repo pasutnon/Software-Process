afaaf925734d26cc64a79d19bf737635c2c415f8
Lemon PS <pattharathon.srithundorn@mail.kmutt.ac.th>
2018-11-14 00:17:23 +0700

    [ADD] back-end

---
+++ back-end/DogeCommerce/src/main/java/com/sit/swprocess/DogeCommerce/DogeCommerceApplication.java
@@ -0,0 +1,12 @@
+package com.sit.swprocess.DogeCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogeCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogeCommerceApplication.class, args);
+	}
+}
