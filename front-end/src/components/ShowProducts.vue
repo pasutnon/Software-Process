dddac874a7a2d0c90623cf1937150d11e234601e
basskt02 <peerawat.02@mail.kmutt.ac.th>
2018-11-16 21:14:09 +0700

    [ADD] Front-end component

---
+++ front-end/src/components/ShowProducts.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>{{name}}</h2>
+        <img alt="Vue logo" src="../assets/logo.png"/>
+        <p>{{productName}} {{productPrice}}</p>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ShowProducts" ,
+        data() {
+            return {
+                name: 'Productss',
+                productName: 'AAA',
+                productPrice: '900'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
