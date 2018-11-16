3fef6006e2b3a851188ce1f5b281ea838984c99c
SandzoNight <flukemdc@gmail.com>
2018-11-16 21:48:21 +0700

    [ADD] AuthenProvider Model and Repository

---
+++ back-end/DogeCommerce/src/main/java/com/sit/swprocess/DogeCommerce/AuthenProvider/AuthenProviderRepository.java
@@ -0,0 +1,6 @@
+package com.sit.swprocess.DogeCommerce.AuthenProvider;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface AuthenProviderRepository extends JpaRepository<AuthenProvider, Long> {
+}
