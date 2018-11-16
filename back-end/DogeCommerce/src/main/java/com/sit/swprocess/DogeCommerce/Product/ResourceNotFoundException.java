343e2c399c99bdaff540427498237ef8b91112bb
GusGus <N.roeksriprapa@gmail.com>
2018-11-16 21:30:29 +0700

    [UPDATE] url mapping in productController

--- back-end/DogeCommerce/src/main/java/com/sit/swprocess/DogeCommerce/Product/ResourceNotFoundException.java
+++ back-end/DogeCommerce/src/main/java/com/sit/swprocess/DogeCommerce/Product/ResourceNotFoundException.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
     private String resourceName;
     private String fieldName;
     private Object fieldValue;
--- back-end/DogeCommerce/src/main/java/com/sit/swprocess/DogeCommerce/ProductImage/ProductImageRepository.java
+++ back-end/DogeCommerce/src/main/java/com/sit/swprocess/DogeCommerce/ProductImage/ProductImageRepository.java
@@ -1,6 +1,5 @@
 package com.sit.swprocess.DogeCommerce.ProductImage;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
