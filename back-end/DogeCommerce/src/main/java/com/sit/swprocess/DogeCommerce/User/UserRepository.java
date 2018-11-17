ac9f2ef02981dcfe3644863eed38d1e543397849
SandzoNight <flukemdc@gmail.com>
2018-11-17 20:38:49 +0700

    [ADD] Handle user not found exception

--- back-end/DogeCommerce/src/main/java/com/sit/swprocess/DogeCommerce/User/UserRepository.java
+++ back-end/DogeCommerce/src/main/java/com/sit/swprocess/DogeCommerce/User/UserRepository.java
@@ -3,7 +3,10 @@ package com.sit.swprocess.DogeCommerce.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     public User findByEmail(String email);
+    public Optional<User> getOneByEmail(String email);
 }
--- back-end/DogeCommerce/src/main/java/com/sit/swprocess/DogeCommerce/User/UserService.java
+++ back-end/DogeCommerce/src/main/java/com/sit/swprocess/DogeCommerce/User/UserService.java
@@ -3,6 +3,8 @@ package com.sit.swprocess.DogeCommerce.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
 
@@ -13,7 +15,7 @@ public class UserService {
         return userRepository.getOne(id);
     }
 
-    public User getUserByEmail(String email) {
-        return userRepository.findByEmail(email);
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.getOneByEmail(email);
     }
 }
