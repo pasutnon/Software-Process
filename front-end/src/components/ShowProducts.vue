2cc7a51fb445d5e43e033468f5a439f39dc76fe6
Sqwitz <napza_1@hotmail.com>
2018-11-18 19:23:08 +0700

    [ADD] Method addProductInCart + saveCart

--- front-end/src/components/ShowProducts.vue
+++ front-end/src/components/ShowProducts.vue
@@ -7,7 +7,7 @@
          <p>SKU Number : {{product.skuNumber}}</p>
          <p>Product Detail : {{product.productDetail}}</p>
          <p>Stock Quantity : {{product.stockQuantity}}</p>
-          <button><img src="../assets/Cart-Orange.svg" height="47"/></button>
+          <button @click="addProductInCart(product)"><img src="../assets/Cart-Orange.svg" height="47"/></button>
         </li>
 
   </ul>
@@ -20,7 +20,8 @@ export default {
   name: "ShowProducts",
   data() {
     return {
-      products: []
+      products: [],
+      cart: []
     };
   },
 
@@ -28,6 +29,31 @@ export default {
     axios
       .get('https://doge-commerce-back-end-grumpy-gecko.mybluemix.net/products')
       .then(response => (this.products = response.data));
+  },
+  methods:{
+        addProductInCart: function (product){
+            let jsonCart = localStorage.getItem('cart')
+            if(!jsonCart) {
+                this.saveCart()
+                return
+            }
+            let cart = JSON.parse(jsonCart)
+            let targetProduct = cart.find((cartProduct) => {
+                return product.productId === cartProduct.productId
+            })
+            if(targetProduct) {
+                targetProduct.quantity += 1
+                this.cart = cart
+            }else {
+                product.quantity = 1
+                this.cart.push(product)
+            }
+            this.saveCart();
+        },
+        saveCart: function(){
+            const parsed = JSON.stringify(this.cart);
+            localStorage.setItem('cart',parsed);
+        }
   }
 };
 </script>
