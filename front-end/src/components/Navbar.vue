6a2190d268c2520d95cc6cc16b7e4ac67f9921d3
phonglyskies <phanuphong.phunklam@mail.kmutt.ac.th>
2018-11-18 15:09:15 +0700

    [ADD] HomeHeader

--- front-end/src/components/Navbar.vue
+++ front-end/src/components/Navbar.vue
@@ -4,21 +4,23 @@
       :value="true"
       absolute
       fixed app
+      height="44px"
+      color="#F4F4F4"
     >
-      <v-btn value="/" color="deep-orange darken-2" flat to="/">
-        <img src="../assets/Home-Orange.svg" height="47"/>
+      <v-btn value="/" color="#F5580C" flat to="/">
+        <img src="../assets/Home-Orange.svg" height="40px"/>
       </v-btn>
 
-      <v-btn value="/search" color="deep-orange darken-2" flat to="/search">
-        <img src="../assets/Category-Orange.svg" height="47"/>
+      <v-btn value="/search" color="#F5580C" flat to="/search">
+        <img src="../assets/Category-Orange.svg" height="40px"/>
       </v-btn>
 
-      <v-btn value="/cart" color="deep-orange darken-2" flat to="/cart">
-        <img src="../assets/Cart-Orange.svg" height="47"/>
+      <v-btn value="/cart" color="#F5580C" flat to="/cart">
+        <img src="../assets/Cart-Orange.svg" height="40px"/>
       </v-btn>
 
-      <v-btn value="/user" color="deep-orange darken-2" flat to="/user">
-        <img src="../assets/User-Orange.svg" height="47"/>
+      <v-btn value="/user" color="#F5580C" flat to="/user">
+        <img src="../assets/User-Orange.svg" height="40px"/>
       </v-btn>
 </v-bottom-nav>
 </template>
--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,9 +1,39 @@
 <template>
-<h1>Home</h1>
+<div style="text-align:center">
+  <p></p>
+  <v-text>สินค้าทั้งหมด</v-text>
+  <p></p>
+<!-- Card layout -->
+   <v-layout>
+    <v-flex xs6 sm6 offset-sm3 style="text-align:center; margin:auto !important;">
+      <v-card>
+        <v-img
+          src="../../นมจืด ดัชมิลล์.png"
+          aspect-ratio="1"
+        ></v-img>
+
+        <v-card-title primary-title>
+          <div>
+            <h2 class="headline mb-0">นมจืด ดัชมิลล์ 180 มล.</h2>
+            <h2 class="headline mb-0" color="#F5580C">฿32</h2>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn color="#F5580C">ใส่รถเข็น</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+  <!-- End Card layout -->
+</div>
 </template>
 <script>
   export default {
-    components: {
+    data () {
+      return {
+        card_text: 'Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.'
+      }
     }
   }
 </script>
