36f75b9a87c937863d880d2e2fa1d826ca6f9f52
Pasut Sriariyanun <pasut.non@mail.kmutt.ac.th>
2018-11-18 14:52:03 +0700

    [UPDATE] ROUTER

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,30 +3,32 @@
     <nav>
         <Navbar></Navbar>
     </nav>
+    
         <router-view></router-view>
+        <ShowProducts></ShowProducts>
 </v-app>
 </template>
 
 <script>
-    import Navbar from './components/Navbar';
-    import Footer from './components/Footer';
-    import ShowProducts from './components/ShowProducts';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import ShowProducts from "./components/ShowProducts";
 
-    export default {
-        name: 'app',
-        components: {
-            ShowProducts,
-            Navbar,
-            Footer
-        }
-    }
+export default {
+  name: "app",
+  components: {
+    ShowProducts,
+    Navbar,
+    Footer
+  }
+};
 </script>
 
 <style>
 nav {
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    margin-bottom: 0 !important;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  margin-bottom: 0 !important;
 }
 </style>
