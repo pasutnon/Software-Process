c9c51b285198e1dcbc7ad50b1752fb9e9ee072da
Unknown <patcharapol.top@mail.kmutt.ac.th>
2018-11-18 23:13:15 +0700

    [ADD] add store navbar

---
+++ front-end/src/store/Navbar.js
@@ -0,0 +1,21 @@
+export const Navbar = {
+    state: {
+      isShowToolBar: true
+    },
+    actions: {
+      setIsShowToolBar: function ({ commit }, isShowToolBar) {
+        commit('setIsShowToolBar', isShowToolBar)
+      }
+    },
+    mutations: {
+      setIsShowToolBar: function (state, isShowToolBar) {
+        state.isShowToolBar = isShowToolBar
+      }
+    },
+    getters: {
+      getIsShowToolBar: function (state) {
+        return state.isShowToolBar
+      }
+    }
+  }
+  
