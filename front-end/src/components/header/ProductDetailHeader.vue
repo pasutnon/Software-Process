e18f4f5caf2f7a7a82ca4c5519759c5d3f4e259d
Unknown <patcharapol.top@mail.kmutt.ac.th>
2018-11-18 23:14:41 +0700

    [UPDATE] update navbar can hide

--- front-end/src/components/header/ProductDetailHeader.vue
+++ front-end/src/components/header/ProductDetailHeader.vue
@@ -1,9 +1,15 @@
 <template>
 <div>
     <v-toolbar color="white" height="50.00px" card>
+        <v-btn icon :to="`${link}`">
+            <v-icon color="#F5580C">arrow_back</v-icon>
+        </v-btn>
         <v-spacer></v-spacer>
-        <h4 class="text-nav">{{link}}</h4>
+        <img src="../../assets/logo.png" width="100"/>
         <v-spacer></v-spacer>
+        <v-btn icon>
+            <v-icon></v-icon>
+        </v-btn>
     </v-toolbar>
     <v-toolbar id="orange-line" color="#F5580C" height="3.75px"></v-toolbar>
 </div>
--- front-end/src/views/ProductDetail.vue
+++ front-end/src/views/ProductDetail.vue
@@ -1,11 +1,25 @@
 <template>
-<div>
-<ProductDetailHeader link="eiei"></ProductDetailHeader>
-<h1>Product Detail {{productID}}</h1>
-</div>
+  <div>
+    <ProductDetailHeader link="/user"></ProductDetailHeader>
+      <b-container>
+        <div class="text-xs-center">
+          <h5>รายละเอียดสินค้า</h5>
+          <img src="../assets/นมจืด ดัชมิลล์.png" width="200" height="200"/>
+        </div>
+        <b-row>
+          <b-col cols="8">test</b-col>
+          <b-col cols="4">test</b-col>
+        </b-row>
+        <p>detail</p>
+      </b-container>
+      <button class="btn-block button-add">
+        เพิ่มสินค้าลงตระกร้า
+      </button>
+  </div>
 </template>
 <script>
 import ProductDetailHeader from '../components/header/ProductDetailHeader';
+import { mapActions } from 'vuex'
   export default {
     components: {
       ProductDetailHeader
@@ -13,13 +27,35 @@ import ProductDetailHeader from '../components/header/ProductDetailHeader';
     data () {
         return {
             productID: '',
+            product:''
         }
     },
     mounted () {
+    this.setIsShowToolBar(false)
     this.productID =
       this.$route.params.productID === undefined
         ? 0
         : this.$route.params.productID
+    },
+    methods:{
+      ...mapActions(['setIsShowToolBar'])
     }
   }
 </script>
+<style>
+.btn-block{
+  position: fixed;
+  bottom: 0;
+  margin-bottom: 0 !important;
+  height: 40px;
+}
+.button-add {
+    background-color: #F5580C; 
+    color: white;
+}
+.button-add:hover {
+    background-color: white;
+    color: #F5580C;
+}
+</style>
+
