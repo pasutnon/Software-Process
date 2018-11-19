e5d033735ba7dd2f071f130579d83817982086b6
Pasut Sriariyanun <pasut.non@mail.kmutt.ac.th>
2018-11-19 21:52:31 +0700

    [UPDATE] Search [FE] form for seaching

--- front-end/src/views/Search.vue
+++ front-end/src/views/Search.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="about">
-    <h1>This is an search page</h1>
+<v-home style="text-align:center !important; magin:auto !important;">
+  <div>
+        <HomeHeader></HomeHeader>
+        <br>
+          <div class="searchform">
+    <h4>Find By Name</h4>
+    <div class="form-group">
+      <input type="text" class="form-control" id="name" required v-model="name" name="name">
+    </div>
+ 
+    <div class="btn-group">
+      <button v-on:click="searchProduct" class="btn btn-success">Search</button>
+    </div>
+ 
+    <ul class="search-result">
+      <li v-for="(product, index) in products" :key="index">
+        <h6>{{product.productName}}</h6>
+      </li>
+    </ul>
   </div>
+        
+  </div>
+</v-home>
 </template>
+
+<script>
+import HomeHeader from "../components/header/HomeHeader";
+
+export default {
+  components: {
+    HomeHeader
+  }
+};
+</script>
