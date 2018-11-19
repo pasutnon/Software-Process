255c6d27c3bd0d585d11c8dfee34208b3de70da5
SandzoNight <flukemdc@gmail.com>
2018-11-19 23:00:05 +0700

    [UPDATE] Get payment controller

--- back-end/DogeCommerce/src/main/java/com/sit/swprocess/DogeCommerce/Payment/PaymentController.java
+++ back-end/DogeCommerce/src/main/java/com/sit/swprocess/DogeCommerce/Payment/PaymentController.java
@@ -1,4 +1,26 @@
 package com.sit.swprocess.DogeCommerce.Payment;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/payments")
 public class PaymentController {
+    @Autowired
+    PaymentService paymentService;
+
+    @GetMapping("")
+    public List<Payment> getAllPayments() {
+        return paymentService.getAllPayments();
+    }
+
+    @GetMapping("/{paymentId}")
+    public Payment getPaymentById(@PathVariable String paymentId) {
+        return paymentService.getPaymentById(paymentId);
+    }
 }
