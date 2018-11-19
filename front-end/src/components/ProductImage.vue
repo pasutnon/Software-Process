a3ece36fccfde20b9c25343da0be7d560a227521
PJ <eye.patinthida@hotmail.com>
2018-11-19 19:14:27 +0700

    [UPDATE] ProductImage

---
+++ front-end/src/components/ProductImage.vue
@@ -0,0 +1,37 @@
+<template>
+        <img
+          :src="path"
+        width="70%"/>      
+
+</template>
+
+<script>
+import axios from "axios"
+import numeral from "numeral"
+
+export default {
+  name: "ProductImage",
+  props:{
+    id:String
+  },
+  data() {
+    return {
+      path: ""
+    };
+  },
+
+  created() {
+    axios
+      .get("https://doge-commerce-back-end-grumpy-gecko.mybluemix.net/products/"+this.id+"/images")
+    .then(response => {
+        console.log(response.data)
+        return this.path = response.data.path
+    });
+  },
+  methods:{}
+}
+</script>
+
+
+<style scoped>
+</style>
