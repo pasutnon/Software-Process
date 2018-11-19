0295f264fc9cb8bce37e3240ab820ff14713f130
SandzoNight <flukemdc@gmail.com>
2018-11-19 23:28:06 +0700

    [ADD] ShipmentMethod

---
+++ back-end/DogeCommerce/src/main/java/com/sit/swprocess/DogeCommerce/Shipment/ShipmentMethodRepository.java
@@ -0,0 +1,6 @@
+package com.sit.swprocess.DogeCommerce.Shipment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShipmentMethodRepository extends JpaRepository<ShipmentMethod, Long> {
+}
