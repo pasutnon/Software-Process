344aba8e0b99cece80bcb63406bbde2799fa3fc6
Pasut Sriariyanun <pasut.non@mail.kmutt.ac.th>
2018-11-20 19:25:11 +0700

    [UPDATE] Cart

--- front-end/src/views/Cart.vue
+++ front-end/src/views/Cart.vue
@@ -1,16 +1,33 @@
 <template>
   <div>
-    <h2>My cart</h2>
-    <div v-for="(product , n) in cart" v-bind:key="product.ID">
-        <p>
-            <span>{{ n+1 }} {{ product.productName }} {{ product.price }} {{ product.quantity }}
-                <v-btn @click="reduceProductInCart(product)">-</v-btn>
+  <div>
+        <HomeHeader></HomeHeader>
+  </div>
+      <v-container
+          fluid
+          grid-list-md
+        >
+    <v-layout row wrap>
+          <v-flex xs6 class="align-items-stretch" v-for="(product, index) in cart" v-bind:key="index">
+              <v-card class="d-flex" style="height: 100%">
+                <div class="align-self-center">
+                  <router-link :to="`/product/detail/${product.productId}`">
+                    <ProductImage :id="product.productId"/>
+                  </router-link>
+                  <div class="product_name">{{product.productName}}</div>
+                  <div class="product_price">฿{{product.price | formatNumber}}</div>
+                  <div class="addProduct">
+                                  <v-btn @click="reduceProductInCart(product)">-</v-btn>
                 <v-btn @click="addProductInToCart(product)">+</v-btn>
-            </span>
             <v-btn @click="removeProductInCart(product)">remove</v-btn>
-        </p>
-        
-    </div>
+                  </div>
+                </div>
+              </v-card>
+          </v-flex>
+    </v-layout>
+  </v-container>
+
+
     <div>
         <span>
             <h2>Total Prices : {{ totalCartPrice }} Baht</h2>
@@ -22,31 +39,34 @@
 </template>
 
 <script>
+import HomeHeader from '../components/header/HomeHeader';
 import { mapMutations, mapGetters } from "vuex";
-
+import axios from "axios";
+import ProductImage from "../components/ProductImage";
+import numeral from "numeral";
+import vue from "vue";
 export default {
+components: {
+    ProductImage,
+    HomeHeader,
+  },
   // name: 'cart',
-  data(){
+  data() {
     return {
-    }        
+    };
   },
-  computed:{
-    ...mapGetters([
-      'totalItemInCart',
-      'totalCartPrice',
-      'cart'
-    ])
+  computed: {
+    ...mapGetters(["totalItemInCart", "totalCartPrice", "cart"])
   },
-  mounted(){
-  },
-  methods:{
+
+  methods: {
     ...mapMutations([
-      'addProductInToCart',
-      'reduceProductInCart',
-      'removeProductInCart',
+      "addProductInToCart",
+      "reduceProductInCart",
+      "removeProductInCart"
     ])
-  }
-}
+  },
+};
 </script>
 
 
