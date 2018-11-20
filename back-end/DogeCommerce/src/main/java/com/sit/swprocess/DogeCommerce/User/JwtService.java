d1bfd9b1244a4072fce28de71cfb3b6d3cc8e1a8
SandzoNight <flukemdc@gmail.com>
2018-11-20 23:11:40 +0700

    [UPDATE] Make JwtService decode only token contains Bearer

--- back-end/DogeCommerce/src/main/java/com/sit/swprocess/DogeCommerce/User/JwtService.java
+++ back-end/DogeCommerce/src/main/java/com/sit/swprocess/DogeCommerce/User/JwtService.java
@@ -4,7 +4,10 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Calendar;
@@ -12,13 +15,23 @@ import java.util.Date;
 
 @Service
 public class JwtService {
+    Logger logger = LoggerFactory.getLogger(JwtService.class);
+
     public static Algorithm algorithmHS = Algorithm.HMAC256("secret");
     public static JWTVerifier verifier = JWT.require(algorithmHS).withIssuer("auth0").build();
     public static JWTCreator.Builder signer = JWT.create().withIssuer("auth0");
 
     public DecodedJWT verify(String token) {
-        DecodedJWT decodedJWT = verifier.verify(token);
-        return decodedJWT;
+        logger.info("incoming Token: "+token);
+        if(token.startsWith("Bearer ")) {
+            token = token.substring(token.indexOf(' ')+1, token.length());
+            logger.info("substringed Token: "+token);
+            DecodedJWT decodedJWT = verifier.verify(token);
+            logger.info("It is "+decodedJWT.getClaim("id").asLong()+" session");
+            return decodedJWT;
+        } else {
+            throw new JWTVerificationException("Token is not containing \"Bearer\"");
+        }
     }
 
     public String encodeUser(User user) {
