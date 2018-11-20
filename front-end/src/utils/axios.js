5f0300a17b66d082d3f02cf3072ca0a3ee5d0a5f
Pureewat Kaewpoy <pureewat.k@gmail.com>
2018-11-20 22:22:36 +0700

    [ADD] header when call api

--- front-end/src/utils/axios.js
+++ front-end/src/utils/axios.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
+import cookie from 'js-cookie'
 
 export default axios.create({
     baseURL: "http://localhost:8081",
-    timeout: 3000
+    timeout: 3000,
+    headers: {
+        'Authorization': 'Bearer ' + cookie.get('session')
+    }
 })
