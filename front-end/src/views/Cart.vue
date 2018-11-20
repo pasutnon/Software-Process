191cc211193d91577cdf10b8dfeaea8c123c698d
Pureewat Kaewpoy <pureewat.k@gmail.com>
2018-11-20 22:44:16 +0700

    [UPDATE] refactor code

--- front-end/src/views/Cart.vue
+++ front-end/src/views/Cart.vue
@@ -1,45 +1,40 @@
 <template>
   <div>
-      <div class="hearder">
-          <v-toolbar color="white" height="50.00px" card>
-              <v-spacer></v-spacer>
-              <h4>รถเข็น</h4>
-              <v-spacer></v-spacer>
-          </v-toolbar>
-          <v-progress-linear class="hr-1" value=100
-                             color="#F5580C" height="3"></v-progress-linear>
-      </div>
-      <br/><br/>
-      <v-container
-          fluid
-          grid-list-md
-        >
-    <v-layout row wrap>
-          <v-flex xs12 class="align-items-stretch" v-for="(product, index) in cart" v-bind:key="index">
-              <v-card class="d-flex" style="height: 100%">
-                <div class="align-self-center">
-                    <b-row>
-                        <b-col cols="4">
-                          <router-link :to="`/product/detail/${product.productId}`">
-                              <div class="text-xs-center">
-                                <ProductImage :id="product.productId"/>
-                              </div>
-                          </router-link>
-                        </b-col>
-                            <b-col cols="8">
-                                <div class="product_name">{{product.productName}}</div>
-                                <div class="addProduct">
-                                    <button @click="reduceProductInCart(product)">-</button>
-                                        {{product.quantity}}
-                                    <button @click="addProductInToCart(product)">+</button>
-                                        <div class="product_price">฿ {{product.price | formatNumber}}</div>
-                                </div>
-                            </b-col>
-                    </b-row>
-                </div>
-              </v-card>
-          </v-flex>
-    </v-layout>
+    <div class="hearder">
+      <v-toolbar color="white" height="50.00px" card>
+        <v-spacer></v-spacer>
+        <h4>รถเข็น</h4>
+        <v-spacer></v-spacer>
+      </v-toolbar>
+      <v-progress-linear class="hr-1" value=100 color="#F5580C" height="3"></v-progress-linear>
+    </div>
+    <v-container class="mt-5">
+      <v-layout row wrap>
+        <v-flex xs12 class="align-items-stretch" v-for="(product, index) in cart" v-bind:key="index">
+            <v-card class="d-flex" style="height: 100%">
+              <div class="align-self-center">
+                <b-row>
+                  <b-col cols="4">
+                    <router-link :to="`/product/detail/${product.productId}`">
+                      <div class="text-xs-center">
+                        <ProductImage :id="product.productId"/>
+                      </div>
+                    </router-link>
+                  </b-col>
+                    <b-col cols="8">
+                      <div class="product_name">{{product.productName}}</div>
+                      <div class="addProduct">
+                        <button @click="reduceProductInCart(product)">-</button>
+                          {{product.quantity}}
+                        <button @click="addProductInToCart(product)">+</button>
+                          <div class="product_price">฿ {{product.price | formatNumber}}</div>
+                      </div>
+                    </b-col>
+                </b-row>
+              </div>
+            </v-card>
+        </v-flex>
+      </v-layout>
   </v-container>
 
 
@@ -50,7 +45,7 @@
             </b-col>
             <b-col cols="5">
                 <div class="text-xs-right">
-                    <router-link to="orderDetail">
+                    <router-link :to="{ name: 'OrderDetail' }">
                         <v-btn>ชำระเงิน ({{ totalItemInCart | formatNumber }})</v-btn>
                     </router-link>
                 </div>
@@ -69,26 +64,26 @@ import ProductImage from "../components/ProductImage";
 import numeral from "numeral";
 import vue from "vue";
 export default {
-components: {
-    ProductImage,
-    HomeHeader,
-  },
-  // name: 'cart',
-  data() {
-    return {
-    };
-  },
-  computed: {
+    components: {
+        ProductImage,
+        HomeHeader,
+    },
+    name: 'cart',
+    data() {
+        return {
+        };
+    },
+    computed: {
     ...mapGetters(["totalItemInCart", "totalCartPrice", "cart"])
-  },
+    },
 
-  methods: {
+    methods: {
     ...mapMutations([
-      "addProductInToCart",
-      "reduceProductInCart",
-      "removeProductInCart"
+        "addProductInToCart",
+        "reduceProductInCart",
+        "removeProductInCart"
     ])
-  },
+    },
 };
 </script>
 <style>
