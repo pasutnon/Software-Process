c1ff8dfbaeee133e8c52b41fff2d8017864dc23a
Pureewat Kaewpoy <pureewat.k@gmail.com>
2018-11-20 22:44:03 +0700

    [UPDATE] using auth from instance of axios

--- front-end/src/middlewares/auth.js
+++ front-end/src/middlewares/auth.js
@@ -2,10 +2,9 @@ import cookies from 'js-cookie'
 import axios from '../utils/axios'
 
 export default async function auth({ next, router }) {
-  axios.defaults.headers.common['Authorization'] = cookies.get('session')
   try {
-    let {data} = await axios.get('users/me')
-  }catch(err) {
+    let {data} = await axios.get('/users/me')
+  } catch(err) {
     cookies.remove('session')
     cookies.remove('userId')
     return router.push({ name: 'user' });
--- front-end/src/utils/axios.js
+++ front-end/src/utils/axios.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 import cookie from 'js-cookie'
 
 export default axios.create({
-    baseURL: "http://localhost:8081",
+    baseURL: process.env.VUE_APP_API_URL,
     timeout: 3000,
     headers: {
-        'Authorization': 'Bearer ' + cookie.get('session')
+        'Authorization': cookie.get('session')
     }
 })
