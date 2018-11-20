15b5702b8a504609ff92ad80764b9c767b97019e
Pasut Sriariyanun <pasut.non@mail.kmutt.ac.th>
2018-11-20 21:01:57 +0700

    [UPDATE] CART

--- front-end/src/views/Cart.vue
+++ front-end/src/views/Cart.vue
@@ -17,10 +17,9 @@
                   <div class="product_name">{{product.productName}}</div>
                   <div class="product_price">฿{{product.price | formatNumber}}</div>
                   <div class="addProduct">
-                  <button @click="reduceProductInCart(product)">-</button>
-                  {{product.quantity}}
-                  <button @click="addProductInToCart(product)">+</button>
-                  <!-- <button @click="removeProductInCart(product)">remove</button> -->
+                                  <v-btn @click="reduceProductInCart(product)">-</v-btn>
+                <v-btn @click="addProductInToCart(product)">+</v-btn>
+            <v-btn @click="removeProductInCart(product)">remove</v-btn>
                   </div>
                 </div>
               </v-card>
@@ -31,56 +30,43 @@
 
     <div>
         <span>
-            <v-card style="color:F5580C;">รวมค่าสินค้า {{ totalCartPrice }} Baht</v-card>
+            <h2>Total Prices : {{ totalCartPrice }} Baht</h2>
             <h2>Total Products : {{ totalItemInCart }}</h2>
         </span>
 
-    </div>
-    <div class="col-7 text-right">
-            <b-button variant="danger"></b-button>
     </div>
     </div>
 </template>
 
 <script>
+import HomeHeader from '../components/header/HomeHeader';
 import { mapMutations, mapGetters } from "vuex";
-
+import axios from "axios";
+import ProductImage from "../components/ProductImage";
+import numeral from "numeral";
+import vue from "vue";
 export default {
+components: {
+    ProductImage,
+    HomeHeader,
+  },
   // name: 'cart',
-  data(){
+  data() {
     return {
-    }        
+    };
   },
-  computed:{
-    ...mapGetters([
-      'totalItemInCart',
-      'totalCartPrice',
-      'cart'
-    ])
+  computed: {
+    ...mapGetters(["totalItemInCart", "totalCartPrice", "cart"])
   },
-  mounted(){
-  },
-  methods:{
+
+  methods: {
     ...mapMutations([
-      'addProductInToCart',
-      'reduceProductInCart',
-      'removeProductInCart',
+      "addProductInToCart",
+      "reduceProductInCart",
+      "removeProductInCart"
     ])
-  }
-}
+  },
+};
 </script>
-<style>
-  button{
-    text-align: center;
-    margin: 2px !important;
-    width: 50px;
-    height: 25px;
-    color: white;
-    background-color:#F5580C; 
-
-  }
-</style>
-
-
 
 
