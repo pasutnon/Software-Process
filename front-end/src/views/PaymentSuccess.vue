58cf89b4eeae72ef7173ff5baa64adeff5660d65
Pasut Sriariyanun <pasut.non@mail.kmutt.ac.th>
2018-11-20 22:12:44 +0700

    [ADD] PaymentSuccess

---
+++ front-end/src/views/PaymentSuccess.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <HomeHeader></HomeHeader>
+    <div class="container shadow-sm rounded bg-white mt-4">
+      <div v-if="isWait">กำลังดึงข้อมูล</div>
+      <div v-else>
+        <div v-if="isComplete"><p style="color:#F5580C; text-align:center; font-size:20px;">ชำระเสร็จสิ้น<br>
+        เลขบิล : </p></div>
+        <div v-else>เกิดข้อผิดพลาด</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HomeHeader from '../components/header/HomeHeader.vue'
+import axios from '../utils/axios.js'
+
+export default {
+  data() {
+    return {
+      isWait: false,
+      isComplete: true,
+    }
+  },
+  components: {
+    HomeHeader,
+  },
+}
+</script>
