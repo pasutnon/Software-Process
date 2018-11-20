806842fa1690415bae845e565ff8a72444538277
Pureewat Kaewpoy <pureewat.k@gmail.com>
2018-11-20 17:02:40 +0700

    [UPDATE] update cart strategy

--- front-end/src/utils/cart.js
+++ front-end/src/utils/cart.js
@@ -5,5 +5,6 @@ export default {
   sumTotalItem: (cart) => cart.reduce((sum, product) => sum + product.quantity, 0),
   sumPrice: (cart) => {
     return cart.reduce((sum, product) => sum + (product.price * product.quantity), 0)
-  }
+  },
+  renewCart: (cart) => JSON.parse(JSON.stringify(cart))
 }
