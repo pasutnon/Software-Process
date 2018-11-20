390432499e86416da232c845689f94c9858eadc1
Pureewat Kaewpoy <pureewat.k@gmail.com>
2018-11-20 14:59:32 +0700

    [ADD] generic header for change some text on header

---
+++ front-end/src/components/header/GenericHeader.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <v-toolbar color="white" height="50.00px" card>
+        <v-spacer></v-spacer>
+        <h4 class="text-nav">{{ header }}</h4>
+        <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-progress-linear class="hr-1" value=100
+    color="#F5580C" height="3"></v-progress-linear>
+</div>
+</template>
+<script>
+export default {
+    name: "GenericHeader",
+    data() {
+        return {
+        }
+    },
+    props: ['header']
+}
+</script>
+<style>
+.text-nav{
+    color: orangered;
+}
+</style>
