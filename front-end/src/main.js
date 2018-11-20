b0696a9594007dd5a36bd219ab400f7e759d9ebd
Unknown <patcharapol.top@mail.kmutt.ac.th>
2018-11-20 18:01:16 +0700

    [UPDATE]  add Modal vue-sweetalert2

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -6,9 +6,11 @@ import store from './store'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueSweetalert2 from 'vue-sweetalert2';
 
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false
+Vue.use(VueSweetalert2);
 
 new Vue({
   router,
