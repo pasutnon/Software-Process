c662a9a97a9ee3e10505ca274852480bf6e19fcc
SandzoNight <flukemdc@gmail.com>
2018-11-20 22:02:52 +0700

    [UPDATE] Refactor User Controller

--- back-end/DogeCommerce/src/main/java/com/sit/swprocess/DogeCommerce/User/UserService.java
+++ back-end/DogeCommerce/src/main/java/com/sit/swprocess/DogeCommerce/User/UserService.java
@@ -4,8 +4,11 @@ import com.sit.swprocess.DogeCommerce.Address.Address;
 import com.sit.swprocess.DogeCommerce.AuthenProvider.AuthenProvider;
 import com.sit.swprocess.DogeCommerce.AuthenProvider.AuthenProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,6 +20,9 @@ public class UserService {
 
     @Autowired
     AuthenProviderService authenProviderService;
+
+    @Autowired
+    JwtService jwtService;
 
     public Optional<User> getUserById(Long id) {
         return userRepository.findById(id);
@@ -41,6 +47,41 @@ public class UserService {
         }
         User user = userRepository.getByAuthenProviders(authenProvider);
         return user;
+    }
+
+    public String signInWithFacebook(String facebookToken, String firstname, String lastname, String email) {
+        Optional<User> userFromEmail = this.getUserByEmail(email);
+        if(userFromEmail.isPresent() == false) {
+            User newUser = this.createNewUserFromFacebook(facebookToken, firstname, lastname, email);
+            String token = jwtService.encodeUser(newUser);
+            String jsonSession = "{\"token\": \""+ token +"\",\"userId\":"+ newUser.getId() +"}";
+            return jsonSession;
+        }else {
+            User user = this.getUserByFacebookToken(facebookToken);
+            if( user != null) {
+                String token = jwtService.encodeUser(user);
+                String jsonSession = "{\"token\": \""+ token +"\",\"userId\":"+ user.getId() +"}";
+                return jsonSession;
+            }
+        }
+        return null;
+    }
+
+    private User createNewUserFromFacebook(String facebookToken, String firstname, String lastname, String email) {
+        AuthenProvider authenProvider = new AuthenProvider("Facebook", facebookToken);
+        List<AuthenProvider> authenProviderList = new ArrayList<>();
+        authenProviderList.add(authenProvider);
+        User user = new User(
+                null,
+                firstname,
+                null,
+                email,
+                firstname,
+                lastname,
+                null,
+                authenProviderList
+        );
+        return this.saveUser(user);
     }
 
 }
